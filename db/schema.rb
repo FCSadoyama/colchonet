6db15440458d0f389c7f9a401ad79c59504e3e34
Felipe <felipe@Mac-mini-de-qcx.local>
2018-12-26 16:38:28 -0200

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_26_124811) do
+ActiveRecord::Schema.define(version: 2018_12_26_181805) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2018_12_26_124811) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.datetime "confirmed_at"
+    t.string "confirmation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
